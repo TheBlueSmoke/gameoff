505193c0d61990e36a13e5cd02a3fd7031132405
Andrew C. Sweet <andrewcsweet@andrews-mbp.granvillehomes.granvillehomes.com>
2018-11-30 16:30:35 -0800

    Enemy Movement

--- game-core/src/system/enemy.rs
+++ game-core/src/system/enemy.rs
@@ -1,18 +1,45 @@
+use amethyst::core::cgmath::InnerSpace;
+use amethyst::core::cgmath::Vector2;
 use amethyst::{
     core::Transform,
     ecs::{Entities, Join, Read, ReadStorage, System, WriteStorage},
     renderer::{SpriteRender, Transparent},
 };
-use crate::component::{Animation, Enemy, Player};
+use crate::component::{Animation, Enemy, Motion, Player};
 use rand::distributions::{Distribution, Uniform};
 
 pub struct Movement;
 
 impl<'s> System<'s> for Movement {
-    type SystemData = (ReadStorage<'s, Enemy>, WriteStorage<'s, Transform>);
+    type SystemData = (
+        ReadStorage<'s, Player>,
+        ReadStorage<'s, Enemy>,
+        WriteStorage<'s, Motion>,
+        WriteStorage<'s, Transform>,
+        Option<Read<'s, crate::map::PassableTiles>>,
+    );
 
-    fn run(&mut self, (enemies, mut transforms): Self::SystemData) {
-        for (_, _transform) in (&enemies, &mut transforms).join() {}
+    fn run(&mut self, (players, enemies, mut motions, mut transforms, passable): Self::SystemData) {
+        if let Some(passable) = passable {
+            let mut player_translation = Vector2 { x: 0.0, y: 0.0 };
+            let mut detection_circle = Vector2 { x: 64.0, y: 64.0};
+
+            // get player position
+            for (_, transform) in (&players, &mut transforms).join() {
+                player_translation = transform.translation.truncate();
+            }
+
+            for (_, transform) in (&enemies, &mut transforms).join() {
+                let enemy_translation = transform.translation.truncate();
+                let player_direction = player_translation - enemy_translation;
+
+                if player_direction.magnitude2() <= detection_circle.magnitude2() {
+                    // let enemy_shift = player_direction - player_direction.normalize();
+                    let enemy_shift = player_direction.normalize();
+                    transform.translation += enemy_shift.extend(0.0);
+                }
+            }
+        }
     }
 }
 
@@ -28,6 +55,7 @@ impl<'s> System<'s> for Spawner {
         WriteStorage<'s, Transparent>,
         Entities<'s>,
         WriteStorage<'s, Animation>,
+        Option<Read<'s, crate::map::PassableTiles>>,
     );
 
     fn run(
@@ -41,45 +69,59 @@ impl<'s> System<'s> for Spawner {
             mut transparent,
             entities,
             mut animation,
+            passable,
         ): Self::SystemData,
     ) {
         let count = (&enemies).join().count();
 
-        if count < 5 {
-            let mut enemy_positions = vec![];
-            let range = Uniform::new_inclusive(-5.0 * 32.0, 5.0 * 32.0);
-            let mut rng = rand::thread_rng();
-            for (_, transform) in (&players, &mut transforms).join() {
-                let mut pos = Transform::default();
-                pos.translation.x = transform.translation.x + range.sample(&mut rng);
-                pos.translation.y = transform.translation.y + range.sample(&mut rng);
+        if let Some(passable) = passable {
+            if count < 5 {
+                let mut enemy_positions = vec![];
+                let range = Uniform::new_inclusive(-5.0 * 32.0, 5.0 * 32.0);
+                let mut rng = rand::thread_rng();
+                for (_, transform) in (&players, &mut transforms).join() {
+                    let mut pos = Transform::default();
+                    pos.translation.x = transform.translation.x + range.sample(&mut rng);
+                    pos.translation.y = transform.translation.y + range.sample(&mut rng);
 
-                enemy_positions.push(pos);
-            }
+                    // get tile and check if passable
+                    let tile_y = (pos.translation.y as u32 / 32) as usize;
+                    let tile_x = (pos.translation.x as u32 / 32) as usize;
 
-            for pos in enemy_positions {
-                let sprite = SpriteRender {
-                    sprite_sheet: textures.textures["penguinFront.png"].clone(),
-                    sprite_number: 0,
-                    flip_horizontal: false,
-                    flip_vertical: false,
-                };
+                    if *passable
+                        .tile_matrix
+                        .get(tile_y)
+                        .and_then(|row| row.get(tile_x))
+                        .unwrap_or(&false)
+                    {
+                        enemy_positions.push(pos);
+                    }
+                }
 
-                let anim = Animation {
-                    total_frames: 2,
-                    max_count_till_next_frame: 0.7,
-                    frame_life_time_count: 0.7,
-                    current_frame: 0,
-                };
+                for pos in enemy_positions {
+                    let sprite = SpriteRender {
+                        sprite_sheet: textures.textures["penguinFront.png"].clone(),
+                        sprite_number: 0,
+                        flip_horizontal: false,
+                        flip_vertical: false,
+                    };
 
-                entities
-                    .build_entity()
-                    .with(pos, &mut transforms)
-                    .with(Enemy::default(), &mut enemies)
-                    .with(sprite, &mut sprites)
-                    .with(Transparent, &mut transparent)
-                    .with(anim, &mut animation)
-                    .build();
+                    let anim = Animation {
+                        total_frames: 2,
+                        max_count_till_next_frame: 0.7,
+                        frame_life_time_count: 0.7,
+                        current_frame: 0,
+                    };
+
+                    entities
+                        .build_entity()
+                        .with(pos, &mut transforms)
+                        .with(Enemy::default(), &mut enemies)
+                        .with(sprite, &mut sprites)
+                        .with(Transparent, &mut transparent)
+                        .with(anim, &mut animation)
+                        .build();
+                }
             }
         }
     }
